26445620f1b353d405b665a98b0a050a9adfddd8
Andrés E. Escobedo G <0228637@up.edu.mx>
2022-03-26 21:26:13 -0600

    Comentado funcion de adaptacion en main

--- Genetico/Genetico.cpp
+++ Genetico/Genetico.cpp
@@ -4,8 +4,8 @@
 int main()
 {
     MaximizacionGenetica Max = MaximizacionGenetica();
-    Max.algoritmo_genetico(30, 0.1, 2, "11010011", 8, 100000);
-    int adaptacion[MAX_SUJETOS] = { 0 };
-    Max.funcion_de_adaptacion(30, 24, adaptacion);
+    Max.algoritmo_genetico(30, 0.1, 2, "11010011", 8, 1000);
+    //int adaptacion[MAX_SUJETOS] = { 0 };      //si se desea ver el valor final de la funcion en el arreglo de adaptacion
+    //Max.funcion_de_adaptacion(30, 24, adaptacion);
     return (0);
 }
